100051df669fad7d0223d37aac11963292be816e
riaosan <fiatpanda750@icloud.com>
2021-05-22 17:55:52 +0900

    fix format

--- src/main.cpp
+++ src/main.cpp
@@ -22,43 +22,46 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#define TS_ENABLE_SSL //Don't forget it for ThingSpeak.h!!
-#include <SerialTelnetBridge.h>
-#include <Ticker.h>
-#include <ESPUI.h>
-#include <TM1637Display.h>
+#define TS_ENABLE_SSL  // Don't forget it for ThingSpeak.h!!
 #include <BME280Class.h>
 #include <Button2.h>
-#include <WiFiClientSecure.h>
-#include <ThingSpeak.h>
-#include <secrets.h>
-#include <esp32_touch.hpp>
+#include <ESPUI.h>
 #include <LED_DisPlay.h>
-//For log
+#include <SerialTelnetBridge.h>
+#include <TM1637Display.h>
+#include <ThingSpeak.h>
+#include <Ticker.h>
+#include <WiFiClientSecure.h>
+#include <secrets.h>
+
+
+#include <esp32_touch.hpp>
+// For log
 #include <esp32-hal-log.h>
-//For WiFi Connection
+// For WiFi Connection
 #define HOSTNAME "atom_clock"
 #define AP_NAME "ATOM-G-AP"
-//For NTP Clock
+// For NTP Clock
 #define TIME_ZONE "JST-9"
 #define NTP_SERVER1 "ntp.nict.jp"
 #define NTP_SERVER2 "ntp.jst.mfeed.ad.jp"
 #define NTP_SERVER3 ""
-//For 7segLED
+// For 7segLED
 #define CLK 19
 #define DIO 22
-//For BME280
+// For BME280
 #define SDA 25
 #define SCL 21
-//For Light Sleep(not use)
-#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 48          /* Time ESP32 will go to sleep (in seconds) */
-//For resetting WiFi
+// For Light Sleep(not use)
+#define uS_TO_S_FACTOR \
+    1000000ULL           /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 48 /* Time ESP32 will go to sleep (in seconds) */
+// For resetting WiFi
 #define BUTTON_PIN 39
-//For PIR Detection
+// For PIR Detection
 #define PIR_SENSOR_PIN 23
-//For Enable LED Display
-#define TOUCH_IO_TOGGLE 8 // GPIO33
+// For Enable LED Display
+#define TOUCH_IO_TOGGLE 8  // GPIO33
 #define TOUCH_THRESHOLD 92
 
 ESP32Touch touch;
@@ -91,11 +94,10 @@ float temperature;
 float humidity;
 float pressure;
 
-void sendThingSpeakChannel(float temperature, float humidity, float pressure)
-{
-    char buffer1[16] = { 0 };
-    char buffer2[16] = { 0 };
-    char buffer3[16] = { 0 };
+void sendThingSpeakChannel(float temperature, float humidity, float pressure) {
+    char buffer1[16] = {0};
+    char buffer2[16] = {0};
+    char buffer3[16] = {0};
 
     sprintf(buffer1, "%2.1f", temperature);
     sprintf(buffer2, "%2.1f", humidity);
@@ -117,9 +119,8 @@ void sendThingSpeakChannel(float temperature, float humidity, float pressure)
         log_d("Problem updating channel. HTTP error code %d", code);
 }
 
-void sendMotionTime(long time)
-{
-    ThingSpeak.setField(4, time / 1000); //ms to sec
+void sendMotionTime(long time) {
+    ThingSpeak.setField(4, time / 1000 / 60);  // ms to min
 
     // write to the ThingSpeak channel
     int code = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
@@ -129,8 +130,7 @@ void sendMotionTime(long time)
         log_d("Problem updating channel. HTTP error code %d", code);
 }
 
-void sendMotionCounts(int counts)
-{
+void sendMotionCounts(int counts) {
     ThingSpeak.setField(5, counts);
 
     // write to the ThingSpeak channel
@@ -141,18 +141,16 @@ void sendMotionCounts(int counts)
         log_d("Problem updating channel. HTTP error code %d", code);
 }
 
-void printTemperatureLED(float value)
-{
-    char buffer[16] = { 0 };
+void printTemperatureLED(float value) {
+    char buffer[16] = {0};
     sprintf(buffer, "0x%2.0fC", value);
 
     display.clear();
     display.showNumberHexEx(strtol(buffer, 0, 16), (0x80 >> 2), false, 3, 1);
 }
 
-void printHumidityLED(float value)
-{
-    char buffer[16] = { 0 };
+void printHumidityLED(float value) {
+    char buffer[16] = {0};
 
     sprintf(buffer, "%2f", value);
     String humidLed(buffer);
@@ -160,9 +158,8 @@ void printHumidityLED(float value)
     display.showNumberDecEx(humidLed.toInt(), (0x80 >> 0), false);
 }
 
-void printPressureLED(float value)
-{
-    char buffer[16] = { 0 };
+void printPressureLED(float value) {
+    char buffer[16] = {0};
 
     sprintf(buffer, "%4f", value);
     String pressLed(buffer);
@@ -170,41 +167,30 @@ void printPressureLED(float value)
     display.showNumberDecEx(pressLed.toInt(), (0x80 >> 0), false);
 }
 
-void _checkSensor(void)
-{
-    sendData = true;
-}
+void _checkSensor(void) { sendData = true; }
 
-String getLEDTime(void)
-{
+String getLEDTime(void) {
     time_t t = time(NULL);
     struct tm* tm = localtime(&t);
 
-    char buffer[16] = { 0 };
+    char buffer[16] = {0};
     sprintf(buffer, "%02d%02d", tm->tm_hour, tm->tm_min);
 
     return String(buffer);
 }
 
-String getTime(void)
-{
+String getTime(void) {
     time_t t = time(NULL);
     struct tm* tm = localtime(&t);
 
-    char buffer[128] = { 0 };
-    sprintf(buffer, "%04d-%02d-%02dT%02d:%02d:%02d+0900",
-            tm->tm_year + 1900,
-            tm->tm_mon + 1,
-            tm->tm_mday,
-            tm->tm_hour,
-            tm->tm_min,
-            tm->tm_sec);
+    char buffer[128] = {0};
+    sprintf(buffer, "%04d-%02d-%02dT%02d:%02d:%02d+0900", tm->tm_year + 1900,
+            tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
 
     return String(buffer);
 }
 
-void displayClock(void)
-{
+void displayClock(void) {
     static uint8_t flag = 0;
     flag = ~flag;
 
@@ -214,44 +200,40 @@ void displayClock(void)
         display.showNumberDecEx(getLEDTime().toInt(), (0x80 >> 4), true);
 }
 
-void initClock(void)
-{
+void initClock(void) {
     configTzTime(TIME_ZONE, NTP_SERVER1, NTP_SERVER2, NTP_SERVER3);
 }
 
-void initESPUI(void)
-{
+void initESPUI(void) {
     ESPUI.setVerbosity(Verbosity::Quiet);
 
     //デバイスの状態
-    ESPUI.addControl(ControlType::Label, "Device IP Address", "", ControlColor::Emerald, Control::noParent);
-    ESPUI.addControl(ControlType::Label, "Device Host Name", "", ControlColor::Sunflower, Control::noParent);
+    ESPUI.addControl(ControlType::Label, "Device IP Address", "",
+                     ControlColor::Emerald, Control::noParent);
+    ESPUI.addControl(ControlType::Label, "Device Host Name", "",
+                     ControlColor::Sunflower, Control::noParent);
 
     //時刻表示パターンの設定
 
-    ESPUI.begin("ESP32 NTP Clock");
+    ESPUI.begin("ATOM NTP Clock");
 }
 
-void displayOn(void)
-{
+void displayOn(void) {
     display.setBrightness(7, true);
     display.clear();
 }
 
-void displayOff(void)
-{
+void displayOff(void) {
     display.setBrightness(7, false);
     display.clear();
 }
 
-void initBME280(void)
-{
+void initBME280(void) {
     bme280.setup(SDA, SCL, MODE::WEATHER_STATION);
     sensorChecker.attach(60, _checkSensor);
 }
 
-void connecting(void)
-{
+void connecting(void) {
     static uint8_t flag = 0;
     flag = ~flag;
 
@@ -261,54 +243,41 @@ void connecting(void)
         display.showNumberDecEx(0, (0x80 >> 4), false);
 }
 
-void released(Button2& btn)
-{
+void released(Button2& btn) {
     WiFi.disconnect(true, true);
     ESP.restart();
 }
 
-void pirDetected(Button2& btn)
-{
+void pirDetected(Button2& btn) {
     detecting = true;
     log_d("--- detected.");
 }
 
-void pirReleased(Button2& btn)
-{
+void pirReleased(Button2& btn) {
     motionTime = btn.wasPressedFor();
     log_d("released: %d", motionTime);
 }
 
-void initButton(void)
-{
-    button.setReleasedHandler(released);
-}
+void initButton(void) { button.setReleasedHandler(released); }
 
-void initPIRSensor(void)
-{
-    pir_sensor.setReleasedHandler(pirReleased);
-}
+void initPIRSensor(void) { pir_sensor.setReleasedHandler(pirReleased); }
 
-void fadeInDisplay(uint32_t ms)
-{
+void fadeInDisplay(uint32_t ms) {
     uint32_t period = ms / 18;
 
     display.setBrightnessEx(0, false);
     delay(period);
 
-    for (int i = 0; i < 8; i++)
-    {
+    for (int i = 0; i < 8; i++) {
         display.setBrightnessEx(i, true);
         delay(period);
     }
 }
 
-void fadeOutDisplay(uint32_t ms)
-{
+void fadeOutDisplay(uint32_t ms) {
     uint32_t period = ms / 18;
 
-    for (int i = 7; -1 < i; i--)
-    {
+    for (int i = 7; - 1 < i; i--) {
         display.setBrightnessEx(i, true);
         delay(period);
     }
@@ -317,20 +286,17 @@ void fadeOutDisplay(uint32_t ms)
     delay(period);
 }
 
-void fadeInOutDisplay(uint32_t ms)
-{
+void fadeInOutDisplay(uint32_t ms) {
     fadeInDisplay(ms);
     fadeOutDisplay(ms);
 }
 
-void initThingSpeak(void)
-{
+void initThingSpeak(void) {
     _client.setCACert(certificate);
     ThingSpeak.begin(_client);
 }
 
-void showEnvData(void)
-{
+void showEnvData(void) {
     printTemperatureLED(temperature);
     fadeInOutDisplay(1.5 * 1000);
 
@@ -341,30 +307,28 @@ void showEnvData(void)
     fadeInOutDisplay(1.5 * 1000);
 }
 
-void sendThingSpeakData(void)
-{
-    if (bme280.getTemperature(temperature) && bme280.getHumidity(humidity) && bme280.getPressure(pressure))
-    {
+void sendThingSpeakData(void) {
+    if (bme280.getTemperature(temperature) && bme280.getHumidity(humidity) &&
+        bme280.getPressure(pressure)) {
         sendThingSpeakChannel(temperature, humidity, pressure);
-    } else
-    {
-        log_e("temperature = %f, humidity = %f, pressure = %f", temperature, humidity, pressure);
+    } else {
+        log_e("temperature = %f, humidity = %f, pressure = %f", temperature,
+              humidity, pressure);
     }
 }
 
-void initTouchSensor(void){
+void initTouchSensor(void) {
     static uint8_t toggle = 0;
 
-    touch.configure_input(TOUCH_IO_TOGGLE, TOUCH_THRESHOLD, [](){
+    touch.configure_input(TOUCH_IO_TOGGLE, TOUCH_THRESHOLD, []() {
         log_d("Toggling Clock LED");
 
-        if (toggle){
+        if (toggle) {
             displayOff();
             showEnvData();
             displayOn();
             clocker.attach_ms(500, displayClock);
-        }
-        else{
+        } else {
             clocker.detach();
             displayOff();
         }
@@ -375,9 +339,8 @@ void initTouchSensor(void){
     touch.begin();
 }
 
-void initLED(void)
-{
-    led.begin(1); //for ATOM Lite
+void initLED(void) {
+    led.begin(1);  // for ATOM Lite
     led.setTaskName("ATOM_LITE_LED");
     led.setTaskPriority(2);
     led.start();
@@ -385,8 +348,7 @@ void initLED(void)
     led.setBrightness(30);
 }
 
-void setup(void)
-{
+void setup(void) {
     initLED();
     led.drawpix(0, CRGB::Red);
 
@@ -416,21 +378,17 @@ void setup(void)
     clocker.attach_ms(500, displayClock);
 }
 
-void loop(void)
-{
-    if (STB.handle() == false)
-    {
+void loop(void) {
+    if (STB.handle() == false) {
         button.loop();
         pir_sensor.loop();
 
-        if (sendData)
-        {
+        if (sendData) {
             sendThingSpeakData();
             sendData = false;
         }
 
-        if (motionTime)
-        {
+        if (motionTime) {
             sendMotionTime(motionTime);
             motionTime = 0;
         }
